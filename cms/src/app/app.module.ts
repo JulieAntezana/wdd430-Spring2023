bae8540954e152c1e682df2adff32d22efe31bad
JulieAntezana <jantezana@byui.edu>
2023-05-11 06:54:35 -0500

    Initial Commit - CMS

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HeaderComponentComponent } from './header.component/header.component.component';
+import { HeaderComponent } from './header.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
@@ -12,7 +11,6 @@ import { ContactDetailComponent } from './contacts/contact-detail/contact-detail
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeaderComponentComponent,
     ContactsComponent,
     ContactListComponent,
     ContactDetailComponent
--- cms/src/app/header.component.ts
+++ cms/src/app/header.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'cms-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {}
 }
