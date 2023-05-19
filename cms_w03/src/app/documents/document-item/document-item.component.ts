839b0a68295dc4082163fa4d13a0ef4c48d8cb85
JulieAntezana <jantezana@byui.edu>
2023-05-18 23:43:52 -0500

    updeate Recipes tutorial W05

--- cms_w03/src/app/documents/document-item/document-item.component.ts
+++ cms_w03/src/app/documents/document-item/document-item.component.ts
@@ -1,15 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-item',
   templateUrl: './document-item.component.html',
   styleUrls: ['./document-item.component.css']
 })
+
 export class DocumentItemComponent implements OnInit {
-  
+  @Input() document: Document;
+  @Output() documentSelected = new EventEmitter<void>();
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
+  onSelected() {
+    this.documentSelected.emit();
+  }
 }
--- cms_w04/src/app/documents/document-list/document-list.component.ts
+++ cms_w04/src/app/documents/document-list/document-list.component.ts
@@ -7,43 +7,52 @@ import { Document } from '../document.model';
   styleUrls: ['./document-list.component.css'],
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter();
   
-  // constructor() {}
+  constructor() {}
   
   
-    documents: Document[] = [
-      {
-        id: '1', 
-        name: 'GS 170 - Career Development', 
-        description: ' Develop resources, professional connections, and essential professional skills to obtain and improve employment.', 
-        url: 'https://byui.instructure.com/courses/245946', children: null 
-      },
-      {
-        id: '2', 
-        name: 'CIT 327 - Data Warehousing', 
-        description: 'This course defines the theory and practice of how database warehouse systems are designed and managed.', 
-        url: 'https://byui.instructure.com/courses/237180',
-        children: null
-      },
-      {
-        id: '3', 
-        name: 'CSE 430 - Architectural Design', 
-        description: 'This class will focus on designing software systems at the largest possible level. An emphasis will be placed on problem solving, analysis skills, and interpersonal social skills.', url: 'https://byui.instructure.com/courses/239224',
-        children: null
-      },
-      {
-        id: '4',
-        name: 'WDD 430 - Web Full-Stack Development', 
-        description: 'This course will teach students how to design and build interactive web based applications using HTML, CSS, JavaScript, and a web development stack.', 
-        url: 'https://byui.instructure.com/courses/244888',
-        children: null
-      },
+    // documents: Document[] = [
+    //   {
+    //     id: '1', 
+    //     name: 'GS 170 - Career Development', 
+    //     description: ' Develop resources, professional connections, and essential professional skills to obtain and improve employment.', 
+    //     url: 'https://byui.instructure.com/courses/245946', children: null 
+    //   },
+    //   {
+    //     id: '2', 
+    //     name: 'CIT 327 - Data Warehousing', 
+    //     description: 'This course defines the theory and practice of how database warehouse systems are designed and managed.', 
+    //     url: 'https://byui.instructure.com/courses/237180',
+    //     children: null
+    //   },
+    //   {
+    //     id: '3', 
+    //     name: 'CSE 430 - Architectural Design', 
+    //     description: 'This class will focus on designing software systems at the largest possible level. An emphasis will be placed on problem solving, analysis skills, and interpersonal social skills.', url: 'https://byui.instructure.com/courses/239224',
+    //     children: null
+    //   },
+    //   {
+    //     id: '4',
+    //     name: 'WDD 430 - Web Full-Stack Development', 
+    //     description: 'This course will teach students how to design and build interactive web based applications using HTML, CSS, JavaScript, and a web development stack.', 
+    //     url: 'https://byui.instructure.com/courses/244888',
+    //     children: null
+    //   },
+    // ];
+
+    documents = [
+      new Document('1', 'Test 1', 'This is test 1.', 'https://www.example.com/1'),
+      new Document('2', 'Test 2', 'This is test 2.', 'https://www.example.com/2'),
+      new Document('3', 'Test 3', 'This is test 3.', 'https://www.example.com/3'),
+      new Document('4', 'Test 4', 'This is test 4.', 'https://www.example.com/4'),
+      new Document('5', 'Test 5', 'This is test 5.', 'https://www.example.com/5'),
     ];
-  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  
   ngOnInit() {}
 
     onSelectedDocument(document: Document) {
-      this.selectedDocumentEvent.emit(document);
+      this.selectedDocumentEvent.emit();
     }
   
 }
